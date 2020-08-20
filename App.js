47dee1ec515a7211b797643adfc715d08b088a09
unknown <rtakanami0706@gmail.com>
2020-08-20 10:40:02 +0900

    コンポーネント作成

--- App.js
+++ App.js
@@ -1,59 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import ListItem from './components/ListItem';
+import articles from './dummies/articles';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   itemContainer: {
     height: 100,
-    width: "100%",
-    borderColor: "gray",
+    width: '100%',
+    borderColor: 'gray',
     borderWidth: 1,
-    flexDirection: "row"
+    flexDirection: 'row',
   },
   leftContainer: {
-    backgroundColor: "red",
-    width: 100
+    backgroundColor: 'red',
+    width: 100,
   },
   rightContainer: {
     flex: 1,
-    flexDirection: "column",
+    flexDirection: 'column',
     padding: 10,
-    justifyContent: "space-between"
+    justifyContent: 'space-between',
   },
   text: {
-    fontSize: 16
+    fontSize: 16,
   },
   subText: {
     fontSize: 12,
-    color: "gray"
-  }
+    color: 'gray',
+  },
 });
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{width: 100, height: 100}}
-            source={{
-              uri: 'https://picsum.photos/id/10/200/200',
-            }}
+    <SafeAreaView  style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) =>
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
           />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </Text>
-          <Text style={styles.subText}>ReactNews</Text>
-        </View>
-      </View>
-    </View>
+        }
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView >
   );
 }
